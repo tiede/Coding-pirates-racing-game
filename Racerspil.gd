e8941123940827f849cf708e23bb04ea1171fba7
Kim Tiedemann <kim@tiede.dk>
2023-02-25 17:24:26 +0100

    Added game over panel with lap times

--- Racerspil.gd
+++ Racerspil.gd
@@ -7,11 +7,11 @@ extends Node2D
 
 var omgangstid = 0
 var omgang = 0
+var omgangstider = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$Gameover.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -26,10 +26,17 @@ func _process(delta):
 		$HUD/Omgang3.text = 'Omgang 3: ' + str(omgangstid).pad_zeros(3).left(7)
 	elif omgang > 3:
 		# Spillet er slut - vi skal lave game over skærm
-		pass
+		$Gameover.visible = true
+		var omgangstider_text = "Omgang 1 : " + str(omgangstider[1]).pad_zeros(3).left(7) + "\n"
+		omgangstider_text += "Omgang 2 : " + str(omgangstider[2]).pad_zeros(3).left(7) + "\n"
+		omgangstider_text += "Omgang 3 : " + str(omgangstider[3]).pad_zeros(3).left(7)
+		$Gameover/Tider.text = omgangstider_text
 		
-	
 func start_passeret():
 	omgang = omgang + 1
+	omgangstider.push_back(omgangstid)
 	omgangstid = 0
 	print('start passeret')
+
+func hent_omgangstid(omgangstid):
+	return str(omgangstid).pad_zeros(3).left(7)
